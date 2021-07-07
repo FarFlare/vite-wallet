667f155043dbd5c4033f5e17dc2a7f40ccfda87f
Anton Tekutov <anton.tekutow@yandex.ru>
2021-07-08 02:41:00 +0300

    MockUp

--- ios/ViteWallet/ProjectViteWallet/ViteWallet/PresentationLayer/UserStories/History/View/HistoryView.swift
+++ ios/ViteWallet/ProjectViteWallet/ViteWallet/PresentationLayer/UserStories/History/View/HistoryView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class HistoryView: TitledView {
     
+    let mockUp = UIImageView(image: .res.historyMockUp())
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -26,12 +28,18 @@ final class HistoryView: TitledView {
     private func setupView() {
         title.text = .res.historyTitle()
         
+        addSubview(mockUp)
+        mockUp.translatesAutoresizingMaskIntoConstraints = false
+        
         makeConstraints()
     }
 
     private func makeConstraints() {
         NSLayoutConstraint.activate([
-            
+            mockUp.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mockUp.leftAnchor.constraint(equalTo: leftAnchor, constant: 24),
+            mockUp.rightAnchor.constraint(equalTo: rightAnchor, constant: -24),
+            mockUp.heightAnchor.constraint(equalTo: mockUp.widthAnchor, multiplier: 518 / 327)
         ])
     }
 }
--- ios/ViteWallet/ProjectViteWallet/ViteWallet/PresentationLayer/UserStories/Wallet/View/WalletView.swift
+++ ios/ViteWallet/ProjectViteWallet/ViteWallet/PresentationLayer/UserStories/Wallet/View/WalletView.swift
@@ -12,6 +12,7 @@ final class WalletView: UIView {
     let title = UILabel()
     let titleLogo = UIImageView(image: .res.walletLogo())
     let glow = GlowView()
+    let mockUp = UIImageView(image: .res.walletMockUp())
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -40,6 +41,9 @@ final class WalletView: UIView {
         title.textColor = .res.tintLight()
         title.text = .res.walletTitle()
         
+        addSubview(mockUp)
+        mockUp.translatesAutoresizingMaskIntoConstraints = false
+        
         makeConstraints()
     }
     
@@ -57,7 +61,11 @@ final class WalletView: UIView {
             glow.centerXAnchor.constraint(equalTo: leftAnchor, constant: .res.screenBounds.width * 0.25),
             glow.centerYAnchor.constraint(equalTo: topAnchor, constant: .res.screenBounds.height * 0.25),
             glow.widthAnchor.constraint(equalToConstant: .res.screenBounds.width),
-            glow.heightAnchor.constraint(equalToConstant: .res.screenBounds.width)
+            glow.heightAnchor.constraint(equalToConstant: .res.screenBounds.width),
+            
+            mockUp.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            mockUp.leftAnchor.constraint(equalTo: leftAnchor),
+            mockUp.rightAnchor.constraint(equalTo: rightAnchor)
         ])
     }
 }
